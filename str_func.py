08fd87d3ef59238cbb710fe084d1973086ee18f9
Arciom Pietroševič <temat31@mail.ru>
2023-12-12 19:08:21 +0300

    add new function on str_func.py

--- str_func.py
+++ str_func.py
@@ -1,3 +1,8 @@
 def lift_up():
     stroka = input()
     print(stroka.upper())
+
+def lift_up_first():
+    """функция делает заглавными первые буквы каждого слова в строке"""
+    stroka = input()
+    print(stroka.title())
